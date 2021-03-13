6d20a34e8232fda2d2e8a7e98a56d6f694cb35d4
Adrian <adihuber12@gmail.com>
2021-03-13 12:33:16 +0100

    add send option for terminal card

--- cards/terminal_card.js
+++ cards/terminal_card.js
@@ -1,23 +1,47 @@
 class TerminalCard extends HTMLElement {
   set hass(hass) {
-    if (!this.content) {
+    this._hass = hass;
+
+    if (!this.console) {
       const card = document.createElement('ha-card');
       card.header = 'Terminal';
-      this.content = document.createElement('div');
-      this.content.style.padding = '0 16px 16px';
-      this.content.style.height = '200px';
-      this.content.style.overflow = 'auto';
-      card.appendChild(this.content);
+
+      this.console = document.createElement('div');
+      this.console.style.padding = '0 16px 16px';
+      this.console.style.height = '200px';
+      this.console.style.overflow = 'auto';
+      card.appendChild(this.console);
+
+      this.form = document.createElement('form');
+      this.input = document.createElement('input');
+      this.input.type = 'text';
+      this.input.style.width = '80%';
+      this.form.appendChild(this.input);
+      this.submit = document.createElement('input');
+      this.submit.type = 'submit';
+      this.submit.value = 'Send';
+      this.submit.style.width = '18%';
+      this.form.appendChild(this.submit);
+      card.appendChild(this.form);
+
       this.appendChild(card);
 
       this.eventData = [];
 
-      hass.connection.subscribeEvents((event) => {
+      this.form.onsubmit = () => {
+        this._hass.callService("esphome", "espprint_send_gcode", {
+          gcode: this.input.value
+        });
+        this.input.value = '';
+        return false;
+      };
+
+      this._hass.connection.subscribeEvents((event) => {
         var date = new Date(event.time_fired);
 
-        this.content.innerHTML += 
+        this.console.innerHTML += 
           "[" + date.toLocaleTimeString('en-US') + "]: " + event.data.method + ": " + event.data.gcode + "<br>";
-          this.content.scrollTop = this.content.scrollHeight;
+          this.console.scrollTop = this.console.scrollHeight;
       }, 'esphome.gcode-event');
     }
   }
