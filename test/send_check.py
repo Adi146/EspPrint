597865d205e6a62aa46f7e2f3467b24a8ce05c04
Adrian <adihuber12@gmail.com>
2021-05-09 13:48:14 +0200

    improve logging of send_check.py

--- test/send_check.py
+++ test/send_check.py
@@ -10,6 +10,23 @@ import threading
 expected_gcodes = []
 REGEX_PATTERN = "^\[.*\]:\sSEND:\sN(\d+)\s(.+)\*(\d+)$"
 
+def setup_logger(name, log_file, format , level=logging.INFO):
+    formatter = logging.Formatter(format)
+
+    fileHandler = logging.FileHandler(log_file, mode='w')        
+    fileHandler.setFormatter(formatter)
+
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(fileHandler)
+    logger.addHandler(streamHandler)
+
+    return logger
+
+
 def uploadFile():
   files = {
     'file': ('integration.gcode', open(sys.argv[1], 'rb'), 'file/application'),
@@ -17,7 +34,8 @@ def uploadFile():
   requests.post('http://espprint_dev/api/files/local', files=files, data={'print': 'true'})
 
 if __name__ == "__main__":
-  logging.basicConfig(filename='send_check.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+  log = setup_logger(name='main', log_file='send_check.log', format='[%(asctime)s][%(levelname)s]: %(message)s')
+  espprint_log = setup_logger(name='espprint', log_file='espprint.log', format='%(message)s')
 
   uploadThread = threading.Thread(target=uploadFile)
   uploadThread.start()
@@ -29,7 +47,7 @@ if __name__ == "__main__":
   expected_gcodes = [gcode for gcode in expected_gcodes if gcode]
 
   for line in sys.stdin:
-    print(line, end='')
+    espprint_log.info(line.strip())
   
     match = re.match(REGEX_PATTERN, line)
     if match:
@@ -37,6 +55,6 @@ if __name__ == "__main__":
       gcode = match.group(2)
 
       if expected_gcodes[0] != gcode:
-        logging.warning("send unexpected gcode: " + gcode + " expected gcode: " + expected_gcodes[0])
+        log.warning("send unexpected gcode: " + gcode + " expected gcode: " + expected_gcodes[0])
       else:
         expected_gcodes = expected_gcodes[1:]
