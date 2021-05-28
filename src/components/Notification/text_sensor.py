a85ce0aa9d65c404f4f2b2bb8d18edc83a22f1b0
Adrian <adihuber12@gmail.com>
2021-05-28 22:56:48 +0200

    fix spacing

--- src/components/Notification/text_sensor.py
+++ src/components/Notification/text_sensor.py
@@ -22,7 +22,7 @@ CONFIG_SCHEMA = (
 )
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield GCodeQueue.register_gcode_analyzer(var, config)
-    yield text_sensor.register_text_sensor(var, config)
+  var = cg.new_Pvariable(config[CONF_ID])
+  yield cg.register_component(var, config)
+  yield GCodeQueue.register_gcode_analyzer(var, config)
+  yield text_sensor.register_text_sensor(var, config)
